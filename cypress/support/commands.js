e78586474e7d3fa9d48260d414d907ca0658da67
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2022-06-26 09:29:53 -0300

    helpers

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,3 +27,24 @@
 import {addMatchImageSnapshotCommand} from 'cypress-image-snapshot/command'
 
 addMatchImageSnapshotCommand()
+
+Cypress.Commands.add('isVisible', (selector)=>{
+  cy.get(selector).should('be.visible');
+});
+Cypress.Commands.add('isHidden', (selector)=>{
+  cy.get(selector).should('not.be.visible'); //not.exist
+});
+Cypress.Commands.add('setResolution', (size)=>{ // [1000, 600] or 'iphone-6'
+  if(Cypress._.isArray(size)){
+    cy.viewport(size[0], size[1])
+  }else if(Cypress._.isString(size)){
+    cy.viewport(size)
+  }
+});
+Cypress.Commands.add('login', (username, password)=>{
+  cy.get('#login_form').should('be.visible');
+  cy.get('#user_login').type(username);
+  cy.get('#user_password').type(password);
+  cy.get('#user_remember_me').click();
+  cy.contains('Sign in').click();
+});
